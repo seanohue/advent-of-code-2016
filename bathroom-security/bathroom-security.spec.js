74b6308124b92e2709362db14a0f8bb849e06ce7
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-03 11:26:35 -0500

    Add tests for day 2 part 1 :cat:

---
+++ bathroom-security/bathroom-security.spec.js
@@ -0,0 +1,15 @@
+const directions = `
+ULL
+RRDDD
+LURDL
+UUUUD
+`
+const BathroomSecurity = require('./bathroom-security');
+
+describe('Keypad', () => {
+
+  it('should be able to reach correct code based on directions', () =>{
+    expect(BathroomSecurity.decode(directions)).to.be('1985');
+  });
+
+});
