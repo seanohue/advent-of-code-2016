f3e833635764695dc5bb7845d4117594b798a1b4
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-03 15:03:56 -0500

    Create a class to handle the keypad business.

--- bathroom-security/bathroom-security.js
+++ bathroom-security/bathroom-security.js
@@ -1,13 +1,36 @@
+const KEYS = [['1', '2', '3'],
+              ['4', '5', '6'],
+              ['7', '8', '9']];
+
+class Keypad {
+  constructor() {
+    this.coords  = {x: 1 , y: 1 };
+    this.pressed = [];
+  }
+
+  get keys() { return KEYS; }
+  get x()    { return this.coords.x; }
+  get y()    { return this.coords.y; }
+  
+  get currentKey() { return this.keys[this.x][this.y]; }
+
+  press()  { return this.pressed.push(this.currentKey); }
+  submit() { return this.pressed.join(); }
+
+}
+
 const decode = directionsStr => {
   const directions = parseDirections(directionsStr);
+  const keypad     = new Keypad();
+  return directions.reduce(walkToKeys, keypad).submit();
 };
 
-const parseDirections = directionsStr => {
-  console.log(directionsStr)
-};
+const walkToKeys = (keypad, direction) => keypad;
+
+const parseDirections = directionsStr => directionsStr.split(/\r?\n/);
 
 const BathroomSecurity = {
-  decode, parseDirections
+  decode, parseDirections, Keypad
 };
 
 module.exports = BathroomSecurity;
--- bathroom-security/bathroom-security.spec.js
+++ bathroom-security/bathroom-security.spec.js
@@ -1,15 +1,21 @@
 const expect = require('chai').expect;
-const directions = `
-ULL
+const directions = `ULL
 RRDDD
 LURDL
 UUUUD
 `
 const BathroomSecurity = require('./bathroom-security');
+const Keypad = BathroomSecurity.Keypad;
+
+describe('Decoder', () => {
+  it('should be able to reach correct code based on directions', () =>{
+    expect(BathroomSecurity.decode(directions)).to.equal('1985');
+  });
+});
 
 describe('Keypad', () => {
-  it('should be able to reach correct code based on directions', () =>{
-    expect(BathroomSecurity.decode(directions)).to.be('1985');
+  it('should instantiate with 5 as the default key', () => {
+    const keys = new Keypad();
+    expect(keys.currentKey).to.equal('5')
   });
-
 });
