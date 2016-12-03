04f9966a09a229b99537b691b2eeefb74bb9657e
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-03 16:08:24 -0500

    I don't know why this isn't working

--- bathroom-security/bathroom-security.js
+++ bathroom-security/bathroom-security.js
@@ -1,31 +1,51 @@
-const KEYS = [['1', '2', '3'],
-              ['4', '5', '6'],
-              ['7', '8', '9']];
-
 class Keypad {
   constructor() {
-    this.coords  = {x: 1 , y: 1 };
+    this.x = 1;
+    this.y = 1;
     this.pressed = [];
   }
 
-  get keys() { return KEYS; }
-  get x()    { return this.coords.x; }
-  get y()    { return this.coords.y; }
-  
-  get currentKey() { return this.keys[this.x][this.y]; }
+  get keys() { return [['1', '2', '3'],
+                       ['4', '5', '6'],
+                       ['7', '8', '9']]; }
+
+  get currentKey() { return this.keys[this.y][this.x]; }
 
   press()  { return this.pressed.push(this.currentKey); }
-  submit() { return this.pressed.join(); }
+  submit() { return this.pressed.join(''); }
+
+  changeKey(x, y) {
+    this.x += x;
+    this.y += y;
+  }
+
+  go(dir) {
+    switch(dir) {
+      case 'R':
+        return this.changeKey(1, 0);
+      case 'L':
+        return this.changeKey(-1, 0);
+      case 'U':
+        return this.changeKey(0, 1);
+      case 'D':
+        return this.changeKey(0, -1);
+    }
+  }
+
+  walk(keypad, direction) {
+  const steps = direction.split('');
+  steps.forEach(keypad.go.bind(keypad));
+  keypad.press();
+  return keypad;
+}
 
 }
 
 const decode = directionsStr => {
   const directions = parseDirections(directionsStr);
   const keypad     = new Keypad();
-  return directions.reduce(walkToKeys, keypad).submit();
+  return directions.reduce(keypad.walk, keypad).submit();
 };
-
-const walkToKeys = (keypad, direction) => keypad;
 
 const parseDirections = directionsStr => directionsStr.split(/\r?\n/);
 
