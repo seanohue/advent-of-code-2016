70fecc4781de99e2e9fca01186c9cbcbe32294a3
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-03 11:39:45 -0500

    Use a collections of functions/reformat fn helpers file.

--- bathroom-security/bathroom-security.js
+++ bathroom-security/bathroom-security.js
@@ -1,14 +1,13 @@
-const BathroomSecurity = {
-
-  decode: (directionsStr) => {
-    const directions = parseDirections(directionsStr);
-  },
-
-  parseDirections: (directionsStr) => {
-    console.log(directionsStr)
-  }
-
+const decode = directionsStr => {
+  const directions = parseDirections(directionsStr);
 };
 
+const parseDirections = directionsStr => {
+  console.log(directionsStr)
+};
+
+const BathroomSecurity = {
+  decode, parseDirections
+};
 
 module.exports = BathroomSecurity;
