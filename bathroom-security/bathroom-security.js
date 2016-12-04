dd4e42e9e779abeaf685c26e9957abcc0fcb9d52
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-03 20:40:35 -0500

    It works! Made it all into just one class and ironed out bugs.

--- bathroom-security/bathroom-security.js
+++ bathroom-security/bathroom-security.js
@@ -1,56 +1,64 @@
 class Keypad {
   constructor() {
-    this.x = 1;
-    this.y = 1;
+    this._x = 1;
+    this._y = 1;
     this.pressed = [];
   }
 
-  get keys() { return [['1', '2', '3'],
-                       ['4', '5', '6'],
-                       ['7', '8', '9']]; }
+  get currentKey() { 
+    const KEYS = [['1', '2', '3'],
+                  ['4', '5', '6'],
+                  ['7', '8', '9']];
+    return KEYS[this.y][this.x]; 
+  }
 
-  get currentKey() { return this.keys[this.y][this.x]; }
+  get x() { return this._x; }
+  get y() { return this._y; }
+  
+  set x(n) { if (this.inbounds(n)) { this._x = n; } }
+  set y(n) { if (this.inbounds(n)) { this._y = n; } }
 
-  press()  { return this.pressed.push(this.currentKey); }
+  inbounds(n) {
+    return (n > -1) && (n < 3);
+  }
+
+  decode(directionsStr) {
+    const directions = this.parseDirections(directionsStr);
+    directions.forEach(dir => this.walk(dir));
+    return this.submit();
+  }
+
+  parseDirections(directionsStr) { return directionsStr.split(/\r?\n/); }
+
+  walk(direction) {
+    const steps = direction.split('');
+    steps.forEach(step => this.go(step));
+    return this.press();
+  }
+
+  press()  { 
+    this.pressed.push(this.currentKey); 
+    return this.currentKey;
+  }
+  
   submit() { return this.pressed.join(''); }
 
+  go(dir) {
+    const coordinateChanges = {
+      'R': [  1,  0 ],
+      'L': [ -1,  0 ],
+      'U': [  0, -1 ],
+      'D': [  0,  1 ]
+    };
+    this.changeKey(...coordinateChanges[dir]);
+    return this.currentKey;
+  }
+  
   changeKey(x, y) {
     this.x += x;
     this.y += y;
   }
 
-  go(dir) {
-    switch(dir) {
-      case 'R':
-        return this.changeKey(1, 0);
-      case 'L':
-        return this.changeKey(-1, 0);
-      case 'U':
-        return this.changeKey(0, 1);
-      case 'D':
-        return this.changeKey(0, -1);
-    }
-  }
-
-  walk(keypad, direction) {
-  const steps = direction.split('');
-  steps.forEach(keypad.go.bind(keypad));
-  keypad.press();
-  return keypad;
 }
 
-}
-
-const decode = directionsStr => {
-  const directions = parseDirections(directionsStr);
-  const keypad     = new Keypad();
-  return directions.reduce(keypad.walk, keypad).submit();
-};
-
-const parseDirections = directionsStr => directionsStr.split(/\r?\n/);
-
-const BathroomSecurity = {
-  decode, parseDirections, Keypad
-};
-
-module.exports = BathroomSecurity;
+module.exports = { Keypad };
--- bathroom-security/bathroom-security.spec.js
+++ bathroom-security/bathroom-security.spec.js
@@ -1,15 +1,15 @@
 const expect = require('chai').expect;
+const Keypad = require('./bathroom-security').Keypad;
+
 const directions = `ULL
 RRDDD
 LURDL
-UUUUD
-`
-const BathroomSecurity = require('./bathroom-security');
-const Keypad = BathroomSecurity.Keypad;
+UUUUD`
 
 describe('Decoder', () => {
   it('should be able to reach correct code based on directions', () =>{
-    expect(BathroomSecurity.decode(directions)).to.equal('1985');
+    const keypad = new Keypad();
+    expect(keypad.decode(directions)).to.equal('1985');
   });
 });
 
@@ -17,5 +17,18 @@ describe('Keypad', () => {
   it('should instantiate with 5 as the default key', () => {
     const keys = new Keypad();
     expect(keys.currentKey).to.equal('5')
+  });
+
+  it('should be able to move keys and return the new key', () => {
+    const keys = new Keypad();
+    const newKey = keys.go('R');
+    expect(newKey).to.equal('6');
+  });
+
+  it('should not wrap or go out of bounds of keyboard', () => {
+    const keys = new Keypad();
+    keys.go('R');
+    const newKey = keys.go('R');
+    expect(newKey).to.equal('6');
   });
 });
