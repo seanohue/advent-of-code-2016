fd8ad755e53c5fc7ac53bfe5d285cea9f0709412
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-05 09:45:16 -0500

    Set up a proper project using mix, get it to compile and run tests :bowtie:

--- triangles/lib/triangles.ex
+++ triangles/lib/triangles.ex
@@ -1,12 +1,11 @@
 defmodule Triangles do
-  @type triangle :: [number, number, number]
+  @type triangle :: [number, ...]
 
   @spec isLegit(triangle) :: boolean
   def isLegit(tri) do
     sorted = Enum.sort(tri)
     largest = List.last(sorted)
-    largest < (Enum.sum(sorted) - last)
+    largest < (Enum.sum(sorted) - largest)
   end
 
 end
-
