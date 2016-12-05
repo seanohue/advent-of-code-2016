31c23c30d77ea94c72ee7f10e15dc661c35276cf
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-05 09:58:16 -0500

    Test the triangle checker for greater good :cherry_blossom:

--- triangles/test/triangles_test.exs
+++ triangles/test/triangles_test.exs
@@ -2,7 +2,14 @@ defmodule TrianglesTest do
   use ExUnit.Case
   doctest Triangles
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "checking good triangles" do
+    goodTriangle = [5, 10, 12]
+    assert Triangles.isLegit(goodTriangle)
   end
+
+  test "checking bad triangles" do
+    badTriangle = [5, 10, 25]
+    assert not Triangles.isLegit(badTriangle)
+  end
+  
 end
