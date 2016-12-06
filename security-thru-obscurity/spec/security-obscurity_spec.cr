6311a42e732948a32460338614023475d8667cde
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-06 12:09:46 -0500

    Write all check room tests first.

--- security-thru-obscurity/spec/security-obscurity_spec.cr
+++ security-thru-obscurity/spec/security-obscurity_spec.cr
@@ -1,7 +1,19 @@
 require "./spec_helper"
 
 describe Security::Obscurity do
-  it "can find a legitimate room" do
-    Security::Obscurity.check_room("aaaaa-bbb-z-y-x-123[abxyz]").should eq true
+  
+  describe "#check_room" do
+    it "can find a legitimate room" do
+      Security::Obscurity.check_room("aaaaa-bbb-z-y-x-123[abxyz]").should eq true
+    end
+
+    it "can find a legit room using tiebreaker" do
+      Security::Obscurity.check_room("a-b-c-d-e-f-g-h-987[abcde]").should eq true
+    end
+
+    it "can find a decoy room" do 
+      Security::Obscurity.check_room("totally-real-room-200[decoy]").should eq false
+    end
   end
+
 end
