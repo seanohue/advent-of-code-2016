0c2ce4c08c1fee00a0700d0d46ce1b6a78b875d2
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-06 15:42:52 -0500

    Create methods to count characters in string.

--- security-thru-obscurity/src/security-obscurity.cr
+++ security-thru-obscurity/src/security-obscurity.cr
@@ -7,19 +7,41 @@ module Security::Obscurity
   def check_room(hash)
     check_sum = get_check_sum(hash)
     encrypted_name = get_encrypted_name(hash, check_sum)
-    true
+    is_legit_room(encrypted_name, check_sum)
   end
+
 
   def get_check_sum(hash)
     hash[/(?<=\[).+?(?=\])/]
   end
 
   def get_encrypted_name(hash, check_sum)
-    hash.chomp("[" + check_sum + "]").chop.chop.chop.delete("-")
+    remove_check_sum(hash, check_sum).chop.chop.chop.delete("-")
+  end
+
+  def is_legit_room(encrypted_name, check_sum)
+    freq = {} of Char => Int32
+    encrypted_name.each_char do |char|  
+      count_char(freq, char)
+    end
+    true
+  end
+
+  def count_char(freq, char)
+    if freq.has_key? char 
+        freq[char] = freq[char] + 1
+      else
+        freq[char] = 1
+      end
+  end
+
+
+  def remove_check_sum(hash, check_sum)
+    hash.chomp("[" + check_sum + "]")
   end
 
   def get_room_number(hash, check_sum)
-    hash.chomp("[" + check_sum + "]")[-3, 3]
+   remove_check_sum(hash, check_sum)[-3, 3]
   end
 
 end
