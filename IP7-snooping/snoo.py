97599f7352396c1ade52d72b3c777fda83b53ddb
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-08 16:31:23 -0500

    Horrible sorcery :high_brightness_

--- IP7-snooping/snoo.py
+++ IP7-snooping/snoo.py
@@ -1,8 +1,24 @@
 """Snooper"""
 
+import re
+
 def find_snoopable(ip_address):
     """
     The IP7 string must contain an ABBA outside of the hypernet []
     and can not contain an ABBA inside of the hypernet []
     """
+    brackets = re.compile(r'\[(.*?)\]')
+    decomposed_ip = brackets.split(ip_address)
+    hypernet = decomposed_ip[1]
+    rest_of_ip = ''.join(list(set(decomposed_ip).difference([hypernet])))
+
+    has_good_abba = find_abba(rest_of_ip)
+    has_bad_abba = find_abba(hypernet)
+
+
+    return has_good_abba and not has_bad_abba
+
+def find_abba(string):
+    """Finds an ABBA pattern in a string and returns True if it exists else False"""
+    print string
     return True
