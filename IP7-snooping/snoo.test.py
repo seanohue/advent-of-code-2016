cf6ce3ec5cb804d6917c6a84b49b41221bfa5d01
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-08 16:34:08 -0500

    Use assertFalse and asserTrue instead, add tests for just finding an abba.

--- IP7-snooping/snoo.test.py
+++ IP7-snooping/snoo.test.py
@@ -7,19 +7,27 @@ class TestSnooper(unittest.TestCase):
 
     def test_find_snoopable(self):
         """Test the ability to find a single snoopable IP."""
-        self.assertEqual(snoo.find_snoopable("abba[mnop]qrst"), True)
+        self.assertTrue(snoo.find_snoopable("abba[mnop]qrst"))
 
     def test_find_unsnoopable_hypernet(self):
         """Test ability to find unsnoopable IP with hypernet [ABBA]."""
-        self.assertEqual(snoo.find_snoopable("abcd[bddb]xyyx"), False)
+        self.assertFalse(snoo.find_snoopable("abcd[bddb]xyyx"))
 
     def test_find_unsnoopable_no_abba(self):
         """Test ability to find unsnoopable IP due to no ABBA."""
-        self.assertEqual(snoo.find_snoopable("aaaa[qwer]tyui"), False)
+        self.assertFalse(snoo.find_snoopable("aaaa[qwer]tyui"))
 
     def test_find_snoopable_nested(self):
         """Test ability to find snoopable IP nested in larger string."""
-        self.assertEqual(snoo.find_snoopable("abba[mnop]qrst"), True)
+        self.assertTrue(snoo.find_snoopable("abba[mnop]qrst"))
+
+    def test_find_any_abba(self):
+        """Test ability to find an ABBA"""
+        self.assertTrue(snoo.find_abba("ABBAYTG"))
+
+    def test_find_not_abba(self):
+        """Finds non-ABBA False"""
+        self.assertFalse(snoo.find_abba("HATS32FGGGGGGGG1"))
 
 if __name__ == '__main__':
     unittest.main()
