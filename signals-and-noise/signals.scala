742626f5a625c76a380788ff0c1b1997b86913ac
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-12-08 13:33:13 -0500

    Problem part two solved, did a bit of a refactor. Still don't really know Scala. :tada:

--- signals-and-noise/signals.scala
+++ signals-and-noise/signals.scala
@@ -13,13 +13,29 @@ object SignalDecoder {
     }
 
     println("DECODED")
-    println(decode(contents))
+    println(decodeMostLikely(contents))
+    println(decodeLeastLikely(contents))
   }
 
-  def decode(noise: String): String = { 
+  def decodeMostLikely(noise:String): String = {
+    return decode(noise, true)
+  }
+
+  def decodeLeastLikely(noise:String): String = {
+    return decode(noise, false)
+  }
+
+  def decode(noise: String, isMostLikely: Boolean): String = {
     val signalMatrix = noise.split(EOL).map(signal => signal.trim.split("")).filter(signal => signal.length > 1)
     val transposed = signalMatrix.transpose
-    val decoded = transposed.map(signal => signal.groupBy(identity).mapValues(_.size).maxBy(_._2)).map(_._1).mkString("")
-    return decoded
+    return if (isMostLikely) findMostLikely(transposed) else findLeastLikely(transposed) 
+  }
+
+  def findMostLikely(transposed: Array[Array[String]]): String = { 
+    return transposed.map(signal => signal.groupBy(identity).mapValues(_.size).maxBy(_._2)).map(_._1).mkString("")
+  }
+
+  def findLeastLikely(transposed: Array[Array[String]]): String = { 
+    return transposed.map(signal => signal.groupBy(identity).mapValues(_.size).minBy(_._2)).map(_._1).mkString("")
   }
 }
